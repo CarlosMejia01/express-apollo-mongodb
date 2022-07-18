5ab03da8906e5348cf1e336fd716197c12a454b3
Carlos Mejia <cmejia@equitel.com.co>
2022-07-18 18:50:55 -0500

    basic apollo express server configuration

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const express = require('express');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+const { typeDefs } = require("./typeDefs");
+const { resolvers } = require("./resolvers");
 
 const app = express();
 
 module.exports = app;
+
+async function start() {
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  //Initialize apollo instance
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({app});
+
+  app.listen(3000, () => {
+    console.log("Server on port", 3000);
+  });
+}
+
+//First execution
+start();
